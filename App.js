0cc1962d49f0f73bfbe3f48da5c94766caf1a786
AutePatil <sagaraute7@gmail.com>
2022-11-02 19:23:44 +0530

    some changes in css and js

--- App.js
+++ App.js
@@ -1,1 +1,24 @@
 
+function Show(id)
+{
+  if (document.getElementById(id).style.display == 'none') {
+       document.getElementById(id).style.display = 'block';
+  }
+  else {
+       document.getElementById(id).style.display = 'none';
+  }
+};
+
+//Submit Button
+function Verify() {
+  var userRef = "Sagarpatil";
+  var passRef = "Sagar3636";
+  
+  var user =  document.getElementById("username").value;
+  var pass = document.getElementById("password").value;
+  if (user == userRef || pass == passRef) {
+  alert("Connecting")
+  } else {
+  alert("you make a mistake...")
+  }
+};
